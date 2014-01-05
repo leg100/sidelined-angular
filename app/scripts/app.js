a7c73b70d08810db0fa39c445da0212bc9d33d43
Louis Garman <louisgarman@gmail.com>
2014-01-05 15:53:56 +0000

    Get tests passing using angular-ui-router

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,13 +1,11 @@
 'use strict';
 
-angular.module('sidelinedApp', [])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
+angular.module('sidelinedApp', ['ui.router'])
+  .config(function ($locationProvider, $stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise("/");
+    $stateProvider
+      .state('root', {
         templateUrl: 'views/main.html',
         controller: 'MainCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
       });
   });
