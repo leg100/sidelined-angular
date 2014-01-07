56fa578d5de3a8a3583c0c48404928af5f8644fc
Louis Garman <louisgarman@gmail.com>
2014-01-07 21:21:46 +0000

    Simple test of Injury.query()

--- test/spec/services/Injury.js
+++ test/spec/services/Injury.js
@@ -3,16 +3,23 @@
 describe('Service: Injury', function () {
 
   // load the service's module
-  beforeEach(module('sidelinedApp'));
+  beforeEach(module('sidelinedApp.injuries'));
 
   // instantiate service
-  var Injury;
-  beforeEach(inject(function (_Injury_) {
+  var Injury, $httpBackend;
+  beforeEach(inject(function (_Injury_, _$httpBackend_) {
     Injury = _Injury_;
+    $httpBackend = _$httpBackend_;
   }));
 
-  it('should do something', function () {
-    expect(!!Injury).toBe(true);
+  it('should retrieve list of all injuries', function () {
+    var promise, result;
+    $httpBackend.expect('GET', '/api/injuries').respond(['p1']);
+    promise = Injury.query();
+    promise.then(function(resp) {
+      result = resp;
+    });
+    $httpBackend.flush();
+    expect(result).toEqual(['p1']);
   });
-
 });
