f6cb0cbf2386f5f43d52ab41f88db5d2633b4019
Louis Garman <louisgarman@gmail.com>
2014-01-20 13:05:12 +0000

    Show domain of source

---
+++ app/scripts/filters/domain.js
@@ -0,0 +1,16 @@
+'use strict';
+
+angular.module('sidelinedApp')
+  .filter('domain', function() {
+    return function (input) {
+      var matches,
+          output = "",
+          urls = /\w+:\/\/([\w|\.]+)/;
+
+      matches = urls.exec( input );
+
+      if ( matches !== null ) output = matches[1];
+
+      return output;
+    };
+  });
