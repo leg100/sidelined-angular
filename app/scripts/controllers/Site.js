88c9addecc5442e254a8dde825b5f26cdc201520
Louis Garman <louisgarman@gmail.com>
2014-01-22 15:18:31 +0000

    State change error is available, if need be

--- app/scripts/controllers/Site.js
+++ app/scripts/controllers/Site.js
@@ -2,7 +2,7 @@
 
 angular.module('sidelinedApp')
   .controller('SiteCtrl', ['$state', '$scope', function ($state, $scope) {
-    $scope.$on('$stateChangeError', function() {
+    $scope.$on('$stateChangeError', function(event, toState, toParams, fromState, fromParams, err) {
       $state.go('error', {}, {location: true, reload: true});
     });
   }]);
