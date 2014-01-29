97897e3e0b4cafe5970435a19befa8f67b35e354
Louis Garman <louisgarman@gmail.com>
2014-01-29 11:04:08 +0000

    log all ui-router errors

--- app/scripts/controllers/Site.js
+++ app/scripts/controllers/Site.js
@@ -3,6 +3,7 @@
 angular.module('sidelinedApp')
   .controller('SiteCtrl', ['$state', '$scope', function ($state, $scope) {
     $scope.$on('$stateChangeError', function(event, toState, toParams, fromState, fromParams, err) {
+      console.dir(err);
       $state.go('error', {}, {location: true, reload: true});
     });
   }]);
